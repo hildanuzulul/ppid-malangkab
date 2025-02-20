bf0f6b0a0ac8a66c127604d7968fea7584f75d14
hildanuzulul <hildanuzulul51@gmail.com>
2025-02-20 20:25:18 +0700

    bootstrap to codeigniter

--- application/controllers/Services.php
+++ application/controllers/Services.php
@@ -1,11 +1,10 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Beranda extends CI_Controller
+class Services extends CI_Controller
 {
-
 	public function index()
 	{
-		$this->load->view('beranda');
+		$this->load->view('services'); 
 	}
 }
--- application/controllers/Testimonials.php
+++ application/controllers/Testimonials.php
@@ -1,11 +1,10 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Beranda extends CI_Controller
+class Testimonials extends CI_Controller
 {
-
 	public function index()
 	{
-		$this->load->view('beranda');
+		$this->load->view('testimonials');
 	}
 }
