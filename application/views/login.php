13fada5905d9effece376b16af204dc1171708e5
Novelia Mega Puspita <noveliamegapuspita@gmail.com>
2025-03-10 10:17:47 +0700

    login

---
+++ application/views/login.php
@@ -0,0 +1,29 @@
+<body>
+	<div class="user-login-wrapper">
+		<div class="user-login-container text-center">
+			<h2>Login</h2>
+			<form>
+				<div class="mb-3 text-start">
+					<label for="username" class="form-label">Username</label>
+					<div class="input-group">
+						<span class="input-group-text"><i class="bi bi-person-fill"></i></span>
+						<input type="text" class="form-control" id="username" placeholder="Enter your username">
+					</div>
+				</div>
+				<div class="mb-3 text-start">
+					<label for="password" class="form-label">Password</label>
+					<div class="input-group">
+						<span class="input-group-text"><i class="bi bi-lock-fill"></i></span>
+						<input type="password" class="form-control" id="password" placeholder="Enter your password">
+					</div>
+				</div>
+				<button type="submit" class="btn btn-sm btn-danger w-100">Login</button>
+			</form>
+		</div>
+
+		<!-- Tambahan teks di bawah login -->
+		<div class="register-container">
+			<p>Belum memiliki akun? <a href="register.html" class="text-danger">Daftar di sini</a></p>
+		</div>
+	</div>
+
