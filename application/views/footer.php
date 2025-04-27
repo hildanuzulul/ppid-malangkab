3b3fd7552fe3ce6304e2755a4e6615f34557bb75
hildanuzulul <hildanuzulul51@gmail.com>
2025-04-27 23:52:09 +0700

    Chatbot

--- application/views/footer.php
+++ application/views/footer.php
@@ -86,6 +86,19 @@
 <!-- Script UserWay -->
 <script src="https://cdn.userway.org/widget.js" data-account="B3mZXfyDkb"></script>
 
+<script type="text/javascript">
+	var Tawk_API = Tawk_API || {},
+		Tawk_LoadStart = new Date();
+	(function() {
+		var s1 = document.createElement("script"),
+			s0 = document.getElementsByTagName("script")[0];
+		s1.async = true;
+		s1.src = 'https://embed.tawk.to/680dd90de41bbb1918cd99a7/1ipr1fnop';
+		s1.charset = 'UTF-8';
+		s1.setAttribute('crossorigin', '*');
+		s0.parentNode.insertBefore(s1, s0);
+	})();
+</script>
 </body>
 
 </html>
