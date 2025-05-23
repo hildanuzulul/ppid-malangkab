d0bb2b0d38910522257e02d90c3177d8a10b312d
Novelia Mega Puspita <noveliamegapuspita@gmail.com>
2025-05-23 21:29:25 +0700

    update berita beranda

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -1,4 +1,5 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class Beranda extends MY_Controller
 {
@@ -10,20 +11,33 @@ class Beranda extends MY_Controller
 
 	public function index()
 	{
-		// Ambil data berita terbatas, misal 5 berita terbaru
-		$url_berita = 'https://web-admin.malangkab.go.id/api/list-berita?id_pd=3&limit=4';
-		$response_berita = file_get_contents($url_berita);
-		$berita_list = json_decode($response_berita, true);
+		// Ambil berita dan gambar dari id_pd=5 (limit 4 untuk beranda)
+		$url_api = 'https://web-admin.malangkab.go.id/api/list-berita?id_pd=5&limit=4';
+		$response = file_get_contents($url_api);
+		$berita_list = json_decode($response, true);
 
-		// Jika ingin pasang gambar default
+		// Siapkan mapping id_artikel => artikel_image_url
+		$gambar_map = [];
+		if (is_array($berita_list)) {
+			foreach ($berita_list as $item) {
+				if (!empty($item['artikel_image_url'])) {
+					$gambar_map[$item['id_artikel']] = $item['artikel_image_url'];
+				}
+			}
+		}
+
+		// Tambahkan gambar ke tiap berita
 		$default_image_url = 'assets/img/logo.png';
 		foreach ($berita_list as &$berita) {
-			$berita['gambar'] = $default_image_url;
+			$id = $berita['id_artikel'];
+			if (isset($gambar_map[$id])) {
+				$berita['gambar'] = 'https://web-admin.malangkab.go.id/5' . $gambar_map[$id];
+			} else {
+				$berita['gambar'] = base_url($default_image_url);
+			}
 		}
 
 		$data['berita'] = $berita_list;
-
-		// Load view dan kirim data berita
 		$this->render('beranda', $data);
 	}
 }
